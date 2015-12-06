8335a551da21fba692f5553b8ad4624f49ca4e5b
fanyou <youfan.noey@gmail.com>
2015-12-06 01:15:00 -0500

    done, TODO read XML and also cupcake war xml file change

--- src/teaching_tool.cpp
+++ src/teaching_tool.cpp
@@ -3,9 +3,11 @@
 
 TeachingTool::TeachingTool(QWidget *parent) :
     ConfigWindowBase(parent),
-    ui(new Ui::TeachingTool)
+    ui(new Ui::TeachingTool),
+    level_list(QStringList(), new QStringListModel(this))
 {
     ui->setupUi(this);
+    ui->levels->setModel(level_list.second);
 }
 
 TeachingTool::~TeachingTool()
@@ -17,8 +19,39 @@ void TeachingTool::ReadXmlFileImp(QString) {
 
 }
 
-void TeachingTool::WriteXmlFileImp(QString) {
+void TeachingTool::WriteXmlFileImp(QString filename) {
+    XmlFileWriter wxml(filename);
+    wxml.setAutoFormatting(true);
 
+    wxml.writeStartElement("ToolCustomization");
+    wxml.writeStartElement("LevelList");
+
+    // lambda for write CData
+    auto WriteCDataNumb = [&wxml](QString tag, double numb) {
+        wxml.writeStartElement(tag);
+        wxml.writeCDATA(QString::number(numb));
+        wxml.writeEndElement();
+    };
+    auto WriteCDataString = [&wxml](QString tag, QString s) {
+        wxml.writeStartElement(tag);
+        wxml.writeCDATA(s);
+        wxml.writeEndElement();
+    };
+
+    for(const auto& ele : levels) {
+        wxml.writeStartElement("level");
+        WriteCDataString("imagepath", ele.imagename);
+        WriteCDataString("imagename", ele.imagepath);
+        WriteCDataNumb("scenenumberofimage", ele.soi);
+        WriteCDataString("textcontent", ele.textcontent);
+        WriteCDataNumb("scenenumberoftext", ele.sot);
+        WriteCDataString("nameofvideo", ele.videoname);
+        WriteCDataString("videourl", ele.videourl);
+        WriteCDataNumb("scenenumberofvideo", ele.sov);
+        wxml.writeEndElement();
+    }
+    wxml.writeEndElement();
+    wxml.writeEndDocument();
 }
 
 bool TeachingTool::CheckEmpty() {
@@ -39,4 +72,64 @@ void TeachingTool::on_load_button_clicked()
     ui->img_path_input->setText(file_name);
     auto sep = file_name.split('/');
     ui->img_name_input->setText(sep[sep.size() - 1]);
+}
+
+void TeachingTool::on_pushButton_2_clicked()
+{
+    // add level
+    if(ui->img_name_input->text() == "") return;
+    levels.emplace_back(ui->img_path_input->text(), ui->img_name_input->text(),
+                        ui->text_content_input->text(), ui->video_name_input->text(), ui->video_url_input->text(),
+                        ui->soi->text().toInt(), ui->sot->text().toInt(), ui->sov->text().toInt());
+    level_list.first << ui->img_name_input->text();
+    level_list.second->setStringList(level_list.first);
+    ui->img_path_input->clear(); ui->img_name_input->clear();
+    ui->text_content_input->clear(); ui->video_url_input->clear();
+    ui->video_name_input->clear();
+    ui->soi->clear();
+    ui->sot->clear();
+    ui->sov->clear();
+}
+
+void TeachingTool::on_levels_clicked(const QModelIndex &index)
+{
+    auto ele = levels[index.row()];
+    ui->img_path_input->setText(ele.imagepath); ui->img_name_input->setText(ele.imagename);
+    ui->text_content_input->setText(ele.textcontent);
+    ui->video_name_input->setText(ele.videoname);
+    ui->video_url_input->setText(ele.videourl);
+    ui->soi->setText(QString::number(ele.soi));
+    ui->sot->setText(QString::number(ele.sot));
+    ui->sov->setText(QString::number(ele.sov));
+}
+
+void TeachingTool::on_pushButton_clicked()
+{
+    // update
+    int idx = ui->levels->currentIndex().row();
+    if(idx < 0) return;
+    levels[idx] = TT_Ele(ui->img_path_input->text(), ui->img_name_input->text(),
+                         ui->text_content_input->text(), ui->video_name_input->text(), ui->video_url_input->text(),
+                         ui->soi->text().toInt(), ui->sot->text().toInt(), ui->sov->text().toInt());
+    ui->img_path_input->clear(); ui->img_name_input->clear();
+    ui->text_content_input->clear(); ui->video_url_input->clear();
+    ui->video_name_input->clear();
+    ui->soi->clear();
+    ui->sot->clear();
+    ui->sov->clear();
+}
+
+void TeachingTool::on_pushButton_3_clicked()
+{
+    int idx = ui->levels->currentIndex().row();
+    if(idx < 0) return;
+    levels.erase(levels.begin() + idx);
+    level_list.first.erase(level_list.first.begin() + idx);
+    level_list.second->setStringList(level_list.first);
+    ui->img_path_input->clear(); ui->img_name_input->clear();
+    ui->text_content_input->clear(); ui->video_url_input->clear();
+    ui->video_name_input->clear();
+    ui->soi->clear();
+    ui->sot->clear();
+    ui->sov->clear();
 }
--- src/teaching_tool.h
+++ src/teaching_tool.h
@@ -8,6 +8,15 @@ namespace Ui {
 class TeachingTool;
 }
 
+struct TT_Ele {
+    TT_Ele(QString ip, QString in, const QString& tc, QString vn, QString vu, int a, int b, int c)
+        : imagepath(ip), imagename(in), textcontent(tc), videoname(vn), videourl(vu),
+          soi(a), sot(b), sov(c){ }
+    QString imagepath, imagename, textcontent, videoname, videourl;
+    int soi, sot, sov;
+    // scene number of image/text/video
+};
+
 class TeachingTool : public ConfigWindowBase
 {
     Q_OBJECT
@@ -24,8 +33,18 @@ public:
 private slots:
     void on_load_button_clicked();
 
+    void on_pushButton_2_clicked();
+
+    void on_levels_clicked(const QModelIndex &index);
+
+    void on_pushButton_clicked();
+
+    void on_pushButton_3_clicked();
+
 private:
     Ui::TeachingTool *ui;
+    std::vector<TT_Ele> levels;
+    std::pair<QStringList, QStringListModel*> level_list;
 };
 
 #endif // TEACHING_TOOL_H
